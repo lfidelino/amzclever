570298f45763850b0bb633a0fd309f5e985f376c
Lawrence Fidelino <lfidelino@gmail.com>
2019-05-15 03:41:21 +0800

    create .eslintignore

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-param-reassign */
+
 /* eslint-disable no-use-before-define */
 // This optional code is used to register a service worker.
 // register() is not called by default.
